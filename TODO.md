4f3abf25a5659e2ec23a9fe1cf84e0122e53ca16
cipriponta <ponta_ciprian@yahoo.com>
2024-03-12 02:16:50 +0200

    [3] Basic functions that handle serial operations related to reading and writing messages

--- TODO.md
+++ TODO.md
@@ -1,2 +1,6 @@
 TODO:
-- Start Step 3
+- Complete the tokenizing part for the eeprom write message command
+- Test that messages can be written in RAM
+- Test that messages can be persisted in EEPROM
+- Update the serial converter
+- Add interface in Flask for displaying messages, adding messages, deleting messages
--- mcu/mcu.ino
+++ mcu/mcu.ino
@@ -1,19 +1,48 @@
-// --- Defines ---
-#define TEMP_SENSOR_PIN             (A0)
-#define LED_PIN                     (13)
-#define FLOOD_SENSOR_PIN            (12)
+#include <EEPROM.h>
 
-#define COMMAND_TURN_LED_ON         ('A')
-#define COMMAND_TURN_LED_OFF        ('S')
-#define COMMAND_GET_TEMPERATURE     ('T')
-#define COMMAND_GET_LED_STATE       ('L')
+// --- Defines ---
+#define TEMP_SENSOR_PIN                         (A0)
+#define LED_PIN                                 (13)
+#define FLOOD_SENSOR_PIN                        (12)
+
+#define COMMAND_TURN_LED_ON                     ('A')
+#define COMMAND_TURN_LED_OFF                    ('S')
+#define COMMAND_GET_TEMPERATURE                 ('T')
+#define COMMAND_GET_LED_STATE                   ('L')
+#define COMMAND_WRITE_MESSAGE                   ('N')
+#define COMMAND_READ_MESSAGES                   ('M')
+
+#define EEPROM_MESSAGE_TABLE_MESSAGE_SIZE       (21)
+#define EEPROM_MESSAGE_TABLE_ENTRIES_SIZE       (10)
+#define EEPROM_MESSAGE_TABLE_START_ADDRESS      (0)
+
+#define SERIAL_MESSAGE_BUFFER_SIZE              (64)
+
+// --- Type Definitions ---
+typedef struct EEPROMMessageEntry
+{
+    uint8_t valid;
+    uint32_t timestamp;
+    char message[EEPROM_MESSAGE_TABLE_MESSAGE_SIZE];
+} EEPROMMessageEntry;
+
+typedef struct EEPROMMessageTable
+{
+    EEPROMMessageEntry entry[EEPROM_MESSAGE_TABLE_ENTRIES_SIZE];
+} EEPROMMessageTable;
 
 // --- Function Prototypes ---
 static double readTemp(void);
 static void setLEDState(uint8_t LEDState);
 static uint8_t getLEDState(void);
 static void readFloodSensorState(void);
+
+static void serialCommandReadMessages(void);
+static void serialCommandWriteMessage(void);
 static void serialManager(void);
+
+// --- Private Variables ---
+static EEPROMMessageTable messageTable = {0};
 
 // --- Function Implementations
 void setup() 
@@ -53,6 +82,31 @@ static void readFloodSensorState(void)
     Serial.println("Flood Sensor State: " + floodSensorStateText);
 }
 
+static void serialCommandReadMessages(void)
+{
+    for(uint8_t message_nr = 0; message_nr < EEPROM_MESSAGE_TABLE_ENTRIES_SIZE; message_nr++)
+    {
+        // Make sure that each message has a string terminator
+        messageTable.entry[message_nr].message[EEPROM_MESSAGE_TABLE_MESSAGE_SIZE - 1] = '\0';
+        Serial.println("Message " + String(message_nr) + 
+                       "|Timestamp " + String(messageTable.entry[message_nr].timestamp) + 
+                       "|" + String(messageTable.entry[message_nr].message));
+    }
+}
+
+static void serialCommandWriteMessage(void)
+{
+    char buffer[SERIAL_MESSAGE_BUFFER_SIZE];
+
+    int bufferLength = Serial.readBytesUntil('!', buffer, SERIAL_MESSAGE_BUFFER_SIZE);
+    if(SERIAL_MESSAGE_BUFFER_SIZE == bufferLength)
+    {
+        bufferLength =  SERIAL_MESSAGE_BUFFER_SIZE - 1;
+    }
+    buffer[bufferLength] = '\0';
+    Serial.println("Buffer: " + String(buffer));
+}
+
 static void serialManager(void)
 {
     if (Serial.available())
@@ -81,6 +135,16 @@ static void serialManager(void)
             {
                 uint8_t LEDState = getLEDState();
                 Serial.println(((LEDState) ? ("ON") : ("OFF")));
+                break;
+            }
+            case COMMAND_READ_MESSAGES:
+            {
+                serialCommandReadMessages();
+                break;
+            }
+            case COMMAND_WRITE_MESSAGE:
+            {
+                serialCommandWriteMessage();
                 break;
             }
             default:
